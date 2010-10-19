2221d9ea184e7ff82fd85f6f98cc84a123e5a042
jDlBot <jdl_bot@yahoo.com>
2010-10-19 15:54:52 -0500

    Added check_update field to config.html template and to server check.

--- src/jdlbotServer.pl
+++ src/jdlbotServer.pl
@@ -478,9 +478,13 @@ END
    '/config' => sub {
 	  my ($httpd, $req) = @_;
 	  if( $req->method() eq 'GET' ){
+		
+		
 		my $configHtml = $templates{'config'}->fill_in(HASH => {'port' => $config{'port'},
 																'jd_address' => $config{'jd_address'},
-																'jd_port' => $config{'jd_port'} });
+																'jd_port' => $config{'jd_port'},
+																'check_update' => $config{'check_update'} eq 'TRUE' ? 'checked="checked"' : ''
+																});
 		$req->respond ({ content => ['text/html', $templates{'base'}->fill_in(HASH => {'title' => 'Configuration', 'content' => $configHtml}) ]});
 	  } elsif ( $req->method() eq 'POST' ){
 		if( $req->parm('action') eq 'update' ){
